62047a9cd0ac48a381f5082b75a897e75af9ed0a
Sujin Hwang <sujinhwang000@gmail.com>
2026-03-26 18:46:55 -0500

    chore: remove temporary ops script from repo

--- restart_mlflow.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pkill -f mlflow
-sleep 2
-nohup /home/ubuntu/mlflow-env/bin/mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root s3://cosmo-gas-vision-storage/mlflow-artifacts --host 0.0.0.0 --port 5000 --allowed-hosts "*" --cors-allowed-origins "*" --serve-artifacts > mlflow.log 2>&1 < /dev/null &
-disown
-exit 0
